af31c82b6674a15715572e250234f8a6e39ae914
Eniko Nagy <4188977+eenagy@users.noreply.github.com>
2024-02-12 14:01:56 +0700

    feat: deployment options

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -11,7 +11,7 @@ const config = {
   favicon: "img/favicon.ico",
 
   // Set the production url of your site here
-  url: "https://debcrafter.com",
+  url: "https://debcrafter.github.io",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: "/",
@@ -19,7 +19,7 @@ const config = {
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: "eenagy", // Usually your GitHub org/user name.
-  projectName: "debcrafter-docs", // Usually your repo name.
+  projectName: "debcrafter.github.io", // Usually your repo name.
 
   onBrokenLinks: "throw",
   onBrokenMarkdownLinks: "warn",
