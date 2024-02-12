c9e483a49808fb1b91391439a742b55ca8def1e2
Eniko Nagy <4188977+eenagy@users.noreply.github.com>
2024-02-12 14:13:40 +0700

    check if name will affect deployment

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -11,7 +11,7 @@ const config = {
   favicon: "img/favicon.ico",
 
   // Set the production url of your site here
-  url: "https://debcrafter.github.io",
+  url: "https://eth-pkg.github.io/debcrafter",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: "/",
